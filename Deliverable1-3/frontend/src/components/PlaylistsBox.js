2f003a554251e5671939c7f7f25fda826676d749
FlipVenter <119102066+FlipVenter@users.noreply.github.com>
2024-09-12 17:42:09 +0200

    Add Docker configuration files and update file paths
    - Added .dockerignore file to exclude unnecessary files from Docker build
    - Updated README.md with instructions to build and start Docker container
    - Created docker-compose.yml file to define Docker services and environment variables
    - Removed background color from home.css
    - Added Dockerfile to build and run the application inside a Docker container
    - Updated image URLs in PlaylistsBox.js to use relative paths

--- Deliverable1-3/frontend/src/components/PlaylistsBox.js
+++ Deliverable1-3/frontend/src/components/PlaylistsBox.js
@@ -4,21 +4,21 @@ import '../../public/assets/home.css'; // Ensure you import your CSS file
 import { Playlists } from './Playlists';
 
 const playlists = [
-    { title: "chill vibes", author: "emma stone", description: "songs to relax and unwind", image: "https://github.com/FlipVenter/IMY220Project/blob/main/Deliverable1-3/frontend/public/assets/images/image.png"},
-    { title: "workout mix", author: "james miller", description: "upbeat tunes to keep you motivated", image: "https://github.com/FlipVenter/IMY220Project/blob/main/Deliverable1-3/frontend/public/assets/images/image.png"},
-    { title: "study beats", author: "sarah brown", description: "instrumental music for focus and concentration", image: "https://github.com/FlipVenter/IMY220Project/blob/main/Deliverable1-3/frontend/public/assets/images/image.png"},
-    { title: "road trip playlist", author: "michael green", description: "songs for a fun and memorable road trip", image: "https://github.com/FlipVenter/IMY220Project/blob/main/Deliverable1-3/frontend/public/assets/images/image.png"},
-    { title: "throwback jams", author: "linda johnson", description: "classic hits to bring back memories", image: "https://github.com/FlipVenter/IMY220Project/blob/main/Deliverable1-3/frontend/public/assets/images/image.png"},
+    { title: "chill vibes", author: "emma stone", description: "songs to relax and unwind", image: "/assets/images/image.png"},
+    { title: "workout mix", author: "james miller", description: "upbeat tunes to keep you motivated", image: "/assets/images/image.png"},
+    { title: "study beats", author: "sarah brown", description: "instrumental music for focus and concentration", image: "/assets/images/image.png"},
+    { title: "road trip playlist", author: "michael green", description: "songs for a fun and memorable road trip", image: "/assets/images/image.png"},
+    { title: "throwback jams", author: "linda johnson", description: "classic hits to bring back memories", image: "/assets/images/image.png"},
 ];
 
 class PlaylistsBox extends React.Component {
     render() {
         return (
-                <div className = "homePlaylistContainer">
-                    {playlists.map((playlist, index) => (
-                        <Playlists key={index} {...playlist} />
-                    ))}
-                </div>
+            <div className = "homePlaylistContainer">
+                {playlists.map((playlist, index) => (
+                    <Playlists key={index} {...playlist} />
+                ))}
+            </div>
         );
     }
 }
--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # IMY220Project
 This will be a playlist sharing website where users can create share and manage their own playlists. 
 
+Run the following command to build the Docker image:
+docker-compose build
 
+Run the following command to start the Docker container:
+docker-compose up
